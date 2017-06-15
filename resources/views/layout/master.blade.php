54088bfaa0c281018c94961f24e20f04d21df064
Adnan Siddiqi <kadnan@gmail.com>
2017-06-15 13:24:49 +0500

    formatting

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Grocery Store a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+    <title>Welcome to Golmarket</title>
     <!-- for-mobile-apps -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
