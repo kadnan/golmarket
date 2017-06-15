657886688b5c51e9234b5751d668265b251f8b6a
Adnan Siddiqi <kadnan@gmail.com>
2017-06-15 13:24:12 +0500

    Adding entry for Dusk Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+	}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		if ( $this->app->environment( 'local', 'testing', 'staging' ) ) {
+			$this->app->register( DuskServiceProvider::class );
+		}
+	}
 }
