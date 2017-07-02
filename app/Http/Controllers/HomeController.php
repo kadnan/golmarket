eab5ff6d518c38944f4d0a356147ebc54bf4f420
Adnan Siddiqi <kadnan@gmail.com>
2017-07-02 14:51:20 +0500

    For part 6

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HotProduct;
+use App\Models\TopProduct;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
 	public function index() {
-		return view('index');
+		$hot_products = HotProduct::join('products','products.id','=','product_id')->get();
+		$top_products = TopProduct::join('products','products.id','=','product_id')->get();
+		return view('index')
+			->with('hot_products',$hot_products)
+			->with('top_products',$top_products);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,4 +30,128 @@
             </ul>
         </div>
     </section>
+    <!-- banner -->
+    <div class="banner_bottom">
+        <div class="wthree_banner_bottom_left_grid_sub">
+        </div>
+        <div class="wthree_banner_bottom_left_grid_sub1">
+            <div class="col-md-4 wthree_banner_bottom_left">
+                <div class="wthree_banner_bottom_left_grid">
+                    <img src="{!! asset('images/4.jpg') !!}" alt=" " class="img-responsive" />
+                    <div class="wthree_banner_bottom_left_grid_pos">
+                        <h4>Discount Offer <span>25%</span></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 wthree_banner_bottom_left">
+                <div class="wthree_banner_bottom_left_grid">
+                    <img src="images/5.jpg" alt=" " class="img-responsive" />
+                    <div class="wthree_banner_btm_pos">
+                        <h3>introducing <span>best store</span> for <i>groceries</i></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 wthree_banner_bottom_left">
+                <div class="wthree_banner_bottom_left_grid">
+                    <img src="images/6.jpg" alt=" " class="img-responsive" />
+                    <div class="wthree_banner_btm_pos1">
+                        <h3>Save <span>Upto</span> $10</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+    <div class="top-brands">
+        <div class="container">
+            <h3>Hot Offers</h3>
+            <div class="agile_top_brands_grids">
+                @foreach($hot_products as $hot_product)
+                    <div class="col-md-3 top_brand_left">
+                        <div class="hover14 column">
+                            <div class="agile_top_brand_left_grid">
+                                <div class="tag"><img src="images/tag.png" alt=" " class="img-responsive" /></div>
+                                <div class="agile_top_brand_left_grid1">
+                                    <figure>
+                                        <div class="snipcart-item block" >
+                                            <div class="snipcart-thumb">
+                                                <a href="single.html"><img width="150" height="150" title=" " alt=" " src="{!! $hot_product->image !!}" /></a>
+                                                <p>{!! $hot_product->title !!}</p>
+                                                <h4>${!! $hot_product->discount_price !!} <span>${!! $hot_product->original_price !!}</span></h4>
+                                            </div>
+                                            <div class="snipcart-details top_brand_home_details">
+                                                <form action="#" method="post">
+                                                    <fieldset>
+                                                        <input type="hidden" name="cmd" value="_cart" />
+                                                        <input type="hidden" name="add" value="1" />
+                                                        <input type="hidden" name="business" value=" " />
+                                                        <input type="hidden" name="item_name" value="Fortune Sunflower Oil" />
+                                                        <input type="hidden" name="amount" value="7.99" />
+                                                        <input type="hidden" name="discount_amount" value="1.00" />
+                                                        <input type="hidden" name="currency_code" value="USD" />
+                                                        <input type="hidden" name="return" value=" " />
+                                                        <input type="hidden" name="cancel_return" value=" " />
+                                                        <input type="submit" name="submit" value="Add to cart" class="button" />
+                                                    </fieldset>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+    </div>
+    <div class="top-brands">
+        <div class="container">
+            <h3>Top Products</h3>
+            <div class="agile_top_brands_grids">
+                @foreach($top_products  as $top_product)
+                    <div class="col-md-3 top_brand_left">
+                        <div class="hover14 column">
+                            <div class="agile_top_brand_left_grid">
+                                <div class="tag"><img src="images/tag.png" alt=" " class="img-responsive" /></div>
+                                <div class="agile_top_brand_left_grid1">
+                                    <figure>
+                                        <div class="snipcart-item block" >
+                                            <div class="snipcart-thumb">
+                                                <a href="single.html"><img width="150" height="150" title=" " alt=" " src="{!! $top_product->image !!}" /></a>
+                                                <p>{!! $top_product->title !!}</p>
+                                                <h4>${!! $top_product->discount_price !!} <span>${!! $top_product->original_price !!}</span></h4>
+                                            </div>
+                                            <div class="snipcart-details top_brand_home_details">
+                                                <form action="#" method="post">
+                                                    <fieldset>
+                                                        <input type="hidden" name="cmd" value="_cart" />
+                                                        <input type="hidden" name="add" value="1" />
+                                                        <input type="hidden" name="business" value=" " />
+                                                        <input type="hidden" name="item_name" value="Fortune Sunflower Oil" />
+                                                        <input type="hidden" name="amount" value="7.99" />
+                                                        <input type="hidden" name="discount_amount" value="1.00" />
+                                                        <input type="hidden" name="currency_code" value="USD" />
+                                                        <input type="hidden" name="return" value=" " />
+                                                        <input type="hidden" name="cancel_return" value=" " />
+                                                        <input type="submit" name="submit" value="Add to cart" class="button" />
+                                                    </fieldset>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+    </div>
+
 @stop
